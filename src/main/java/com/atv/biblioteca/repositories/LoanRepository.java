fbc2ea5890c02fccb698f3007d65de1745328c95
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-03-06 16:45:58 -0300

    feat: add loan repository

--- src/main/java/com/atv/biblioteca/repositories/LoanRepository.java
+++ src/main/java/com/atv/biblioteca/repositories/LoanRepository.java
@@ -1,9 +1,9 @@
 package com.atv.biblioteca.repositories;
 
-import com.atv.biblioteca.entities.LibraryWalletEntity;
+import com.atv.biblioteca.entities.LoanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LibraryWalletRepository extends JpaRepository<LibraryWalletEntity, Long> {
+public interface LoanRepository extends JpaRepository<LoanEntity, Long> {
 }
