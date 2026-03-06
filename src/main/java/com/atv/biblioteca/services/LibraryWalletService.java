705c20fd867b25d019f482715b4e414f3d4b8808
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-03-06 15:27:31 -0300

    feat: add user wallet validation

--- src/main/java/com/atv/biblioteca/services/LibraryWalletService.java
+++ src/main/java/com/atv/biblioteca/services/LibraryWalletService.java
@@ -1,9 +1,12 @@
 package com.atv.biblioteca.services;
 
 import com.atv.biblioteca.entities.LibraryWalletEntity;
+import com.atv.biblioteca.entities.UserEntity;
 import com.atv.biblioteca.repositories.LibraryWalletRepository;
+import com.atv.biblioteca.repositories.UserRepository;
 import com.atv.biblioteca.requests.LibraryWalletRequestDTO;
 import com.atv.biblioteca.responses.LibraryWalletResponseDTO;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +15,24 @@ import java.time.LocalDateTime;
 @Service
 public class LibraryWalletService {
     private final LibraryWalletRepository libraryWalletRepository;
+    private final UserRepository userRepository;
 
-    public LibraryWalletService(LibraryWalletRepository libraryWalletRepository){
+    public LibraryWalletService(LibraryWalletRepository libraryWalletRepository, UserRepository userRepository){
         this.libraryWalletRepository = libraryWalletRepository;
+        this.userRepository = userRepository;
     }
 
-    public ResponseEntity<LibraryWalletResponseDTO> createLibraryWallet(LibraryWalletRequestDTO lwreqdto){
-        if(lwreqdto == null){
-            throw new IllegalArgumentException("os dados estão inválidos");
+    public LibraryWalletResponseDTO createLibraryWallet(Long userId){
+        UserEntity user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("usuário não encontrado"));
+        if(user.getLibraryWallet() != null){
+            throw new IllegalStateException("esse usuário já possui uma carteirinha");
         }
-        LibraryWalletEntity libraryWallet = new LibraryWalletEntity(lwreqdto.getLibraryWalletNumber(), LocalDateTime.now());
+        LibraryWalletEntity libraryWallet = new LibraryWalletEntity();
+        libraryWallet.setEmissionDate(LocalDateTime.now());
+        libraryWallet.setValid(true);
+        user.setLibraryWallet(libraryWallet);
+        libraryWallet.setUser(user);
         LibraryWalletEntity newLibraryWallet = libraryWalletRepository.save(libraryWallet);
-        return new LibraryWalletResponseDTO();
+        return new LibraryWalletResponseDTO(newLibraryWallet);
     }
 }
